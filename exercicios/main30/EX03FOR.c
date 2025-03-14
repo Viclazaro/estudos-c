2b6f7cfbfff35470e951de6a247a7a65e501cf83
Yen <thearyazitah@gmail.com>
2025-03-14 11:44:23 -0300

    exercicios

---
+++ exercicios/main30/EX03FOR.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    //imprima todos os numeros de 20 a 50
+
+    int i;
+    for ( i = 20; i <= 50; i++)
+    {
+        printf("%d\n", i);
+    }
+    
+   return 0;
+}
